86ede21d93d13e58380de3ef8dbbaa00803c3649
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-03-24 00:18:57 +0530

    Add files via upload

---
+++ ES6/var_let.js
@@ -0,0 +1,11 @@
+var a=3;
+var a=15;
+console.log(a);    //15
+
+let m=3;
+let m=7;
+console.log(m);   //Syntax error: Identifier 'm' has already been declared
+
+const x=10;
+x=90;
+console.log(x);    //TypeError: assigned to constant variable
